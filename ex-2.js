567caa3dd775b95ad9695c10107d81e370107a70
Puttapong <puttapong.lekk@gmail.com>
2024-05-15 14:09:20 +0700

    ex1-ex7

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,13 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Off") {
+  console.log("Light bulb is Off.");
+} else if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+  lightBulbStatus = "Light bulb is Broken.";
+} else {
+  console.log("Please choose the correct input (On/Off/Broken)");
+}
